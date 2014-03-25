812982bcb167006da670a60b33f39d041c953c9e
gbergy <greg.goldberg718@gmail.com>
2014-03-25 14:59:26 -0400

    assign 3b revised

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssignThreeb::Application.config.secret_key_base = 'c75403521d705bbedf620fb9c32351fec17a7773f7953459ea643ea499469e6afaa7291386742c4ad8efb67d5dc76fb63dabe7a70fe1a8dafaf9c4d367f801d2'
